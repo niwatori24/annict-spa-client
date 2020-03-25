d9862b1f7954a5571c963b14d7b5711421c9ae7a
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-03-25 16:32:29 +0900

    Remove css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import { Menu } from './Menu'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Menu></Menu>
-        <div>
-          <p>Annict SPA Client</p>
-        </div>
-      </header>
+      <div>
+        header menu
+      </div>
+      <Menu></Menu>
+      <div>
+        <p>Annict SPA Client</p>
+      </div>
     </div>
   );
 }
