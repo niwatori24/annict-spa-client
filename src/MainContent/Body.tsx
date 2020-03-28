942926e15632bea8e35b3bd6e30d0232eb7218af
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-03-28 16:55:12 +0900

    Define type

--- src/MainContent/Body.tsx
+++ src/MainContent/Body.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function MainContentBody() {
+export const MainContentBody: React.FC = () => {
   return (
     <div style={{ flex: 1, background: 'yellow' }}>コンテンテンツ2</div>
   )
--- src/MainContent/Menu.tsx
+++ src/MainContent/Menu.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function MainContentMenu() {
+export const MainContentMenu: React.FC = () => {
   return (
     <div style={{ flex: 1, background: 'red' }}>メニュー2</div>
   )
