c9c6e14b03f455d31810285617faf0a76b13e6e6
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-03-28 19:06:43 +0900

    一覧APIをメニューにそのまま表示する

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 import { MainContentMenu } from './MainContent/Menu'
 import { MainContentBody} from './MainContent/Body'
+import axios from 'axios';
 
-export function MainContent() {
+interface Work {
+  id: number
+  title: string
+};
+
+export const MainContent: React.FC = () => {
+  const [workList, setWorkList] = useState<Work[]>([]);
+  const getWorks = () => {
+    var url = 'https://api.annict.com/v1/works?access_token=fdow1gkjI71ZbDLZj8IOzpYZYvuo8O7vKhd3vKKDzuI'
+    axios.get(url, {}).then((res) => {
+      const list: Work[] = [];
+      res.data.works.map((w: any, i: number) => {
+        list.push({ id: w.id, title: w.title });
+      });
+      console.log('done getWorks!! with:', list);
+      setWorkList((prev) => {
+        return list;
+      });
+
+      // console.log('complate request.data: ', workList);
+    }).catch(console.error);
+  };
+
+  useEffect(() => {
+    getWorks();
+  });
+
   return (
     <div style={{ display: 'flex', width: '700px' }}>
-      <MainContentMenu />
+      <MainContentMenu workList={workList} />
       <MainContentBody />
     </div>
   )
--- src/MainContent/Menu.tsx
+++ src/MainContent/Menu.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
-export const MainContentMenu: React.FC = () => {
+interface Work {
+  id: number
+  title: string
+};
+
+interface Props {
+  workList: Work[]
+}
+
+export const MainContentMenu: React.FC<Props> = props => {
+  console.log(props.workList);
   return (
-    <div style={{ flex: 1, background: 'red' }}>メニュー2</div>
+    <div style={{ flex: 1, background: 'red' }}>
+      リストです
+      <ul>
+        { props.workList.map((work, i) => {
+           console.log('work', work);
+           return <li>{work.title}</li>
+          })
+        }
+      </ul>
+    </div>
   )
 }
