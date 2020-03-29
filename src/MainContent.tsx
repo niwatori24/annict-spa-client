27413ecc1eacd806897835e8b9a85acd1c574ab2
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-03-29 16:51:23 +0900

    Use const

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -14,7 +14,7 @@ export const MainContent: React.FC = () => {
   const [currentWork, setCurrentWork] = useState({ id: 0, title: '' });
 
   const getWorks = () => {
-    var url = `https://api.annict.com/v1/works?access_token=${process.env.REACT_APP_ANNICT_API_TOKEN}`
+    const url = `https://api.annict.com/v1/works?access_token=${process.env.REACT_APP_ANNICT_API_TOKEN}`
     axios.get(url, {}).then((res) => {
       const list: Work[] = [];
       res.data.works.map((w: any, i: number) => {
