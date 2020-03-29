9b22dd3627283a23ab0df013f1e3a85e0023c2bf
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-03-29 16:01:05 +0900

    空のBodyを切り出す

--- src/MainContent/Body.tsx
+++ src/MainContent/Body.tsx
@@ -13,9 +13,7 @@ export const MainContentBody: React.FC<Props> = props => {
         <p>id: {props.currentWork.id}<br />
         title: {props.currentWork.title}</p>
       )}
-      {props.currentWork.id === 0 && (
-        <p>ここにタイトルが表示されます</p>
-      )}
+      {props.currentWork.id === 0 && <EmptyBody />}
     </div>
   )
 }
---
+++ src/MainContent/EmptyBody.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const EmptyBody: React.FC = () => {
+  return (
+    <p>ここにタイトルが表示されます</p>
+  )
+}
