8728fe1405471beea1b8351e4f61ee4079da5406
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-03-29 20:59:09 +0900

    レスポンスに含まれ情報を表示する

--- src/MainContent/Body.tsx
+++ src/MainContent/Body.tsx
@@ -3,21 +3,33 @@ import React, {
   useEffect,
   useState
 } from 'react';
+// components
+import { PaginationComponent } from '../PaginationComponent'
+import { AnnictAPI } from '../AnnictAPI'
 import { EmptyBody } from './EmptyBody'
 import { MainContentEpisode } from './Episode'
+// types
 import { Work } from './../types/Work'
 import { Episode } from './../types/Episode'
+// libs
 import axios from 'axios'
-import { AnnictAPI } from './../AnnictAPI'
 
 interface Props {
   currentWork: Work | null
 }
+interface Pagination {
+  totalCount: number
+  nextPage: number | null
+  prevPage: number | null
+}
+type PaginationType = Pagination | null
 
 type EpisodeList = Episode[] | null
 
 export const MainContentBody: React.FC<Props> = props => {
   const [episodes, setEpisodes] = useState<EpisodeList>([])
+  const [pagination, setPagination] = useState<PaginationType>(null)
+
   useEffect(() => {
     setEpisodes((prev) => { return null });
     getEpisodes()
@@ -28,6 +40,12 @@ export const MainContentBody: React.FC<Props> = props => {
     const url: string = AnnictAPI.episodesUrl(props.currentWork.id)
     axios.get(url, {}).then((res) => {
       let list: EpisodeList = [];
+      console.log('episodeUrl res.data:', res.data)
+      setPagination((prev) => ({
+        totalCount: res.data.total_count,
+        nextPage: res.data.next_page,
+        prevPage: res.data.prev_page
+      }))
       res.data.episodes.map((w: any, i: number) => {
         if(list === null) { return }
         list.push({ id: w.id, title: w.title })
@@ -43,6 +61,7 @@ export const MainContentBody: React.FC<Props> = props => {
           <div>id: {props.currentWork.id}</div>
           <div>title: {props.currentWork.title}</div>
           <MainContentEpisode episodes={episodes} />
+          <PaginationComponent pagination={pagination} />
         </div>
       )}
       {props.currentWork === null && <EmptyBody />}
