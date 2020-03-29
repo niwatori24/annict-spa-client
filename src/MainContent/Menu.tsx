751ee2bed4b0ca61f7f088959683019d72999137
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-03-29 18:00:05 +0900

    onclickイベントの伝搬をとめる

--- src/MainContent/Menu.tsx
+++ src/MainContent/Menu.tsx
@@ -9,7 +9,6 @@ interface Props {
 export const MainContentMenu: React.FC<Props> = props => {
   const handleClick = (work: Work) => {
     props.setCurrentWork(() => ({ id: work.id, title: work.title }))
-    return false
   }
 
   return (
@@ -18,7 +17,7 @@ export const MainContentMenu: React.FC<Props> = props => {
         { props.workList.map((work, i) => {
             return <li
               key={work.id}
-              onClick={() => { handleClick(work)  }}
+              onClick={(e) => { handleClick(work); e.preventDefault(); }}
             ><a href='#'>{work.title}</a></li>
           })
         }
