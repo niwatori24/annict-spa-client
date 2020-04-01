66c346785b18087681babe138d60f620d0e5637f
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-04-01 16:16:04 +0900

    エピソードの話数を表示する

--- src/MainContent/Body.tsx
+++ src/MainContent/Body.tsx
@@ -47,9 +47,9 @@ export const MainContentBody: React.FC<Props> = props => {
         nextPage: res.data.next_page,
         prevPage: res.data.prev_page
       }))
-      res.data.episodes.map((w: any, i: number) => {
+      res.data.episodes.map((ep: any, i: number) => {
         if(list === null) { return }
-        list.push({ id: w.id, title: w.title })
+        list.push({ id: ep.id, title: ep.title, episode_number: ep.number, episode_text: ep.number_text })
       });
       setEpisodes((prev) => (list))
     }).catch(console.error);
