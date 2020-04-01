88d1511c606967ee095d709bf28e4395113a0b93
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-04-01 16:43:12 +0900

    エピソードの並びを昇順にする

--- src/AnnictAPI.ts
+++ src/AnnictAPI.ts
@@ -6,9 +6,9 @@ interface AnnictAPIType {
 export const AnnictAPI: AnnictAPIType = {
   episodesUrl: (workId: number, page: number | null) => {
     if(page === null) {
-      return `https://api.annict.com/v1/episodes?access_token=${process.env.REACT_APP_ANNICT_API_TOKEN}&filter_work_id=${workId}`
+      return `https://api.annict.com/v1/episodes?sort_sort_number=desc&access_token=${process.env.REACT_APP_ANNICT_API_TOKEN}&filter_work_id=${workId}`
     } else {
-      return `https://api.annict.com/v1/episodes?access_token=${process.env.REACT_APP_ANNICT_API_TOKEN}&filter_work_id=${workId}&page=${page}`
+      return `https://api.annict.com/v1/episodes?sort_sort_number=asc&access_token=${process.env.REACT_APP_ANNICT_API_TOKEN}&filter_work_id=${workId}&page=${page}`
     }
   },
   worksUrl: () => {
