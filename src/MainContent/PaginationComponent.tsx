d427194df8d7433135ee8831ca1cf65ac2dca98c
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-04-01 15:50:36 +0900

    ページがある時ページネーションのテキストを表示する

--- src/MainContent/PaginationComponent.tsx
+++ src/MainContent/PaginationComponent.tsx
@@ -13,14 +13,14 @@ type PaginationType = Pagination | null
 export const PaginationComponent: React.FC<Props> = props => {
   return (
     <div>
-      {props.pagination && (
-        <div>全ページ数: {props.pagination.totalCount}</div>
-      )}
-      {props.pagination && (
-        <div>
-          {props.pagination.prevPage && (<div>前のエピソードを見る</div>)}
-          {props.pagination.nextPage && (<div>次のエピソードを見る</div>)}
-        </div>
+      {props.pagination && props.pagination.totalCount > 0 && (
+        <>
+          <div>全ページ数: {props.pagination.totalCount}</div>
+          <div>
+            {props.pagination.prevPage && (<div>前のエピソードを見る</div>)}
+            {props.pagination.nextPage && (<div>次のエピソードを見る</div>)}
+          </div>
+        </>
       )}
     </div>
   )
