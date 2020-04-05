7f6e00c597d7aeda42c6abde3a3537795055e748
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-04-05 23:41:51 +0900

    Switch redux

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -2,6 +2,7 @@ import React, {
   useState,
   useEffect,
   useCallback,
+  useReducer,
   useRef
 } from 'react';
 import { MainContentMenu } from './MainContent/Menu'
@@ -10,31 +11,55 @@ import axios from 'axios';
 import { Work } from './types/Work'
 import { AnnictAPI } from './AnnictAPI'
 
+// const reducer2 = (state: any): <Work>[] => {
+// console.log(state)
+// }
+
+function reducer(state: Work[], action: any): Work[]  {
+  console.log('from reducer', state, action)
+  switch (action.type) {
+    case 'getWork':
+      console.log('has return')
+      let list = getWorks()
+      console.log('getWorks() =>', list)
+      return getWorks()
+  }
+  console.log('empty return')
+  return []
+}
+
+const async getWorks = (): Work[] => {
+  const url: string = AnnictAPI.worksUrl()
+  const res = await axios.get(url, {})
+  const list: Work[] = []
+  res.data.works.map((w: any, i: number) => {
+    list.push({ id: w.id, title: w.title })
+  })
+  console.log('another thread in getWorks')
+  return list
+  console.log('main thread in getWorks')
+  return []
+}
+
+export function getWorkType() {
+  return { type: 'getWork' }
+}
+
 export const MainContent: React.FC = () => {
-  const [workList, setWorkList] = useState<Work[]>([]);
   const [currentWork, setCurrentWork] = useState(null);
 
-  const getWorks = () => {
-    const url: string = AnnictAPI.worksUrl()
-    axios.get(url, {}).then((res) => {
-      const list: Work[] = [];
-      res.data.works.map((w: any, i: number) => {
-        list.push({ id: w.id, title: w.title });
-      });
-      setWorkList((prev) => {
-        return list;
-      });
-    }).catch(console.error);
-  };
+  const [workList2, workListDispatch] = useReducer(reducer, [])
+  const getWorkList = useCallback(() => workListDispatch(getWorkType()), [])
 
   useEffect(() => {
-    getWorks();
+    console.log('run getWorks()')
+    getWorkList()
   }, [])
 
   return (
     <div style={{ display: 'flex', width: '700px' }}>
       <MainContentMenu
-        workList={workList}
+        workList={workList2}
         setCurrentWork={setCurrentWork}
       />
       <MainContentBody currentWork={currentWork} />
