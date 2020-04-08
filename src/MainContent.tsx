598154a6fe876a0c2f9fdec3b5bb098cd7a74ffd
niwatori24 <62463593+niwatori24@users.noreply.github.com>
2020-04-08 18:40:25 +0900

    workListにuseReducerを使う

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,9 +1,7 @@
 import React, {
   useState,
   useEffect,
-  useCallback,
-  useReducer,
-  useRef
+  useReducer
 } from 'react';
 import { MainContentMenu } from './MainContent/Menu'
 import { MainContentBody} from './MainContent/Body'
@@ -11,34 +9,13 @@ import axios from 'axios';
 import { Work } from './types/Work'
 import { AnnictAPI } from './AnnictAPI'
 
-// const reducer2 = (state: any): <Work>[] => {
-// console.log(state)
-// }
-
 function reducer(state: Work[], action: any): Work[]  {
-  console.log('from reducer', state, action)
   switch (action.type) {
     case 'getWork':
-      console.log('has return')
-      let list = getWorks()
-      console.log('getWorks() =>', list)
-      return getWorks()
+      return action.payload
+    default:
+      return []
   }
-  console.log('empty return')
-  return []
-}
-
-const async getWorks = (): Work[] => {
-  const url: string = AnnictAPI.worksUrl()
-  const res = await axios.get(url, {})
-  const list: Work[] = []
-  res.data.works.map((w: any, i: number) => {
-    list.push({ id: w.id, title: w.title })
-  })
-  console.log('another thread in getWorks')
-  return list
-  console.log('main thread in getWorks')
-  return []
 }
 
 export function getWorkType() {
@@ -48,18 +25,24 @@ export function getWorkType() {
 export const MainContent: React.FC = () => {
   const [currentWork, setCurrentWork] = useState(null);
 
-  const [workList2, workListDispatch] = useReducer(reducer, [])
-  const getWorkList = useCallback(() => workListDispatch(getWorkType()), [])
+  const [workList, workListDispatch] = useReducer(reducer, [])
 
   useEffect(() => {
-    console.log('run getWorks()')
-    getWorkList()
+    const url: string = AnnictAPI.worksUrl()
+    axios.get(url, {}).then((res) => {
+      const list: Work[] = []
+      res.data.works.map((w: any, i: number) => {
+        list.push({ id: w.id, title: w.title })
+      })
+      workListDispatch({ type: getWorkType().type, payload: list })
+      return list
+    })
   }, [])
 
   return (
     <div style={{ display: 'flex', width: '700px' }}>
       <MainContentMenu
-        workList={workList2}
+        workList={workList}
         setCurrentWork={setCurrentWork}
       />
       <MainContentBody currentWork={currentWork} />
